7bf6d631904df29e036881dc404c27f9891e5536
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-15 01:48:05 +0300

    feat: add course #4

---
+++ course-04/course-04.cs
@@ -0,0 +1,79 @@
+﻿public class Solution
+{
+
+    public static int Factorial(int n)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+
+        int factorial = 1;
+
+        for (int i = n; i > 0; i--)
+        {
+            factorial *= i;
+        }
+
+        return factorial;
+    }
+
+    public static float GetDaysCount(int numberOfHours)
+    {
+        if (numberOfHours == 0)
+        {
+            return 0;
+        }
+
+        return numberOfHours / (float)24;
+    }
+
+    public static float GetWeeksCount(int numberOfHours)
+    {
+        if (numberOfHours == 0)
+        {
+            return 0;
+        }
+
+        return GetDaysCount(numberOfHours) / 7;
+    }
+
+    public static int MinutesToSeconds(int minutes)
+    {
+        return (minutes * 60);
+    }
+
+    public static int HoursToMinutes(int hours)
+    {
+        return (hours * 60);
+    }
+
+    public static int DaysToHours(int days)
+    {
+        return (days * 24);
+    }
+
+    public static int HoursToSeconds(int hours)
+    {
+        return MinutesToSeconds(HoursToMinutes(hours));
+    }
+
+    public static int DaysToSeconds(int days)
+    {
+        return HoursToSeconds(DaysToHours(days));
+    }
+
+    public static int GetTotalSeconds(int days, int hours, int minutes, int seconds)
+    {
+        return DaysToSeconds(days) + HoursToSeconds(hours) + MinutesToSeconds(minutes) + seconds;
+    }
+
+    private static void Main()
+    {
+        // Console.WriteLine(Factorial(6));
+        // Console.WriteLine(GetWeeksCount(365).ToString("N2"));
+        // Console.WriteLine(GetDaysCount(365).ToString("N2"));
+
+        Console.WriteLine(GetTotalSeconds(2, 5, 45, 35).ToString("N0"));
+    }
+}
