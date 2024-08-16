6bbdf5b16e0183d159e55f56532bc41acd28fee1
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-16 23:09:28 +0300

    feat(course #5): add course #5

---
+++ course-05/course-05.cs
@@ -0,0 +1,45 @@
+﻿public class Solution
+{
+    public static bool IsPrime(int number)
+    {
+        if (number <= 1)
+        {
+            return false;
+        }
+
+        if (number == 2 || number == 3)
+        {
+            return true;
+        }
+
+        if (number % 2 == 0 || number % 3 == 0)
+        {
+            return false;
+        }
+
+        //double sqrtNumber = Math.Sqrt(number);
+
+        //for (int i = 5; i <= sqrtNumber; i += 6)
+        //{
+        //    if ((number % i == 0) || (number % (i + 2) == 0))
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        for (int i = 5; i * i <= number; i += 6)
+        {
+            if ((number % i == 0) || (number % (i + 2) == 0))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static void Main()
+    {
+        Console.WriteLine(IsPrime(29));
+    }
+}
