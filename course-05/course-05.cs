1141c026cfb118d1c219cd3256f9e3422e8c1d48
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-16 23:22:41 +0300

    feat(course #5): add a new question

--- course-05/course-05.cs
+++ course-05/course-05.cs
@@ -38,8 +38,28 @@
         return true;
     }
 
+    public static bool IsPerfect(int number)
+    {
+        if (number <= 0)
+        {
+            return false;
+        }
+
+        int sum = 0;
+        for (int i = 1; i <= number / 2; i++)
+        {
+            if (number % i == 0)
+            {
+                sum += i;
+            }
+        }
+
+        return (sum == number);
+    }
+
     private static void Main()
     {
-        Console.WriteLine(IsPrime(29));
+        // Console.WriteLine(IsPrime(29));
+        Console.WriteLine(IsPerfect(29));
     }
 }
